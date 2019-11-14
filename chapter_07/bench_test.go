c197ca21ff6dba23e89f71fe0cf47b4be31115e7
Andrey Sorokin <a40in@inbox.ru>
2019-11-14 18:29:41 +0200

    added bench

--- chapter_07/bench_test.go
+++ chapter_07/bench_test.go
@@ -7,24 +7,50 @@ import (
 // go test -bench=.
 // go test -bench=. -benchmem
 
-func BenchmarkSlice(b *testing.B) {
-	var testSlice []int
-	for i := 0; i < 102400; i++ {
-		testSlice = append(testSlice, i)
-	}
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		returnFromSlice(testSlice, 102399)
-	}
-}
+// func BenchmarkSlice(b *testing.B) {
+// 	var testSlice []int
+// 	for i := 0; i < 102400; i++ {
+// 		testSlice = append(testSlice, i)
+// 	}
+// 	b.ResetTimer()
+// 	for i := 0; i < b.N; i++ {
+// 		returnFromSlice(testSlice, 102399)
+// 	}
+// }
 
-func BenchmarkMap(b *testing.B) {
-	testMap := make(map[int]int)
-	for i := 0; i < 102400; i++ {
-		testMap[i] = i
-	}
+// func BenchmarkMap(b *testing.B) {
+// 	testMap := make(map[int]int)
+// 	for i := 0; i < 102400; i++ {
+// 		testMap[i] = i
+// 	}
+// 	b.ResetTimer()
+// 	for i := 0; i < b.N; i++ {
+// 		returnFromMap(testMap, 102399)
+// 	}
+// }
+
+func BenchmarkDijckstra(b *testing.B) {
+	g := newGraph()
+
+	a1 := g.addNode("1")
+	b2 := g.addNode("2")
+	c3 := g.addNode("3")
+	d4 := g.addNode("4")
+	e5 := g.addNode("5")
+	f6 := g.addNode("6")
+
+	g.addEdge(a1, b2, 7)
+	g.addEdge(a1, c3, 9)
+	g.addEdge(a1, f6, 14)
+	g.addEdge(b2, c3, 10)
+	g.addEdge(b2, d4, 15)
+	g.addEdge(c3, d4, 11)
+	g.addEdge(f6, c3, 2)
+	g.addEdge(e5, f6, 9)
+	g.addEdge(d4, e5, 6)
+
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		returnFromMap(testMap, 102399)
+		g.dijkstra(a1)
 	}
 }
--- chapter_07/dijckstra_2.go
+++ chapter_07/dijckstra_2.go
@@ -1,12 +1,17 @@
-// Версия с представлением узлов и ребер в виде хэш таблиц
+// Версия с представлением узлов и ребер в виде хэш таблиц в 17 раз быстрее версии с обходами узлов и ребер в циклах
+// и это при небольшом графе. Вообще не требует аллокаций памяти.
+// goos: windows
+// goarch: amd64
+// pkg: github.com/NuclearLouse/grokking_algorithms_Aditya_Bhargava/chapter_07
+// BenchmarkDijckstra-2     5024041               222 ns/op               0 B/op          0 allocs/op
+// PASS
+// ok      github.com/NuclearLouse/grokking_algorithms_Aditya_Bhargava/chapter_07  1.496s
 package main
 
 import (
 	"fmt"
 	"math"
 )
-
-var inf = math.Inf(1)
 
 type graph struct {
 	nodes
@@ -45,7 +50,7 @@ func newGraph() *graph {
 }
 
 func (g *graph) addNode(n string) node {
-	nn := node{name: n, cost: inf, status: true}
+	nn := node{name: n, cost: math.Inf(1), status: true}
 	g.nodes[n] = nn
 	return nn
 }
@@ -63,9 +68,10 @@ func (g *graph) addNeighbour(n node, nn string) {
 	g.nodes[n.name] = node{old.name, old.cost, old.status, append(old.neighbour, nn)}
 }
 
-func (g *graph) setCost(n node, c float64) {
+func (g *graph) setCost(n node, c float64) node {
 	old := g.nodes[n.name]
 	g.nodes[n.name] = node{old.name, c, old.status, old.neighbour}
+	return g.nodes[n.name]
 }
 
 func (g *graph) offNode(n node) {
@@ -73,26 +79,73 @@ func (g *graph) offNode(n node) {
 	g.nodes[n.name] = node{old.name, old.cost, false, old.neighbour}
 }
 
-func (g *graph) findLowCosts(n node) {
+func (g *graph) checkEdge(p1, p2 string) float64 {
+	if e, ok := g.edges[p1+"-"+p2]; ok {
+		return e.weight
+	}
+	return g.edges[p2+"-"+p1].weight
+}
 
+func minCost(n []node) node {
+	if len(n) == 2 {
+		if n[0].cost < n[1].cost {
+			return n[0]
+		}
+		return n[1]
+	}
+	min := minCost(n[1:]) //рекурсия
+	if n[0].cost < min.cost {
+		return n[0]
+	}
+	return min
+}
+
+func (g *graph) findLowCosts(n node) {
+	var listNeigbours []node                // объявляю будущий список соседей для поиска мин.
+	neighbours := g.nodes[n.name].neighbour // получаю список соседей начальной ноды
+	for i := range neighbours {             // начинаю обход списка
+		neighbour := g.nodes[neighbours[i]] // выбираю i-го соседа
+		if !neighbour.status {              // проверяю, чтоб он был активен
+			continue // если отключен иду к другому
+		}
+		cw := g.nodes[n.name].cost + g.checkEdge(n.name, neighbours[i])
+		if neighbour.cost > cw { // сравниваю стоимость соседа со стоимостью база+ребро
+			neighbour = g.setCost(neighbour, cw) // если сосед дороже, то делаю его дешевле
+		}
+		listNeigbours = append(listNeigbours, neighbour) // добавляю соседа в список соседей для поиска мин.
+	} // повторяю со всеми соседями
+	g.offNode(n) // отключаю базу
+
+	if len(listNeigbours) <= 1 { // прекращаю если в списке 1 или меньше узлов
+		return
+	}
+	lowNode := minCost(listNeigbours) // нахожу соседа с минимальной стоимостью
+	g.findLowCosts(lowNode)           // рекурсивно повторяю поиск соседей для найденного минимального узла
 }
 
 func (g *graph) dijkstra(start node) {
-	g.setCost(start, 0)
-	g.findLowCosts(start)
+	n := g.setCost(start, 0)
+	g.findLowCosts(n)
 }
 
 func main() {
 	g := newGraph()
-	a := g.addNode("a")
-	b := g.addNode("b")
-	c := g.addNode("c")
-	d := g.addNode("d")
-	g.addEdge(a, b, 65)
-	g.addEdge(a, c, 74)
-	g.addEdge(c, b, 33)
-	g.addEdge(c, d, 40)
-	g.addEdge(b, d, 50)
+	a1 := g.addNode("1")
+	b2 := g.addNode("2")
+	c3 := g.addNode("3")
+	d4 := g.addNode("4")
+	e5 := g.addNode("5")
+	f6 := g.addNode("6")
+
+	g.addEdge(a1, b2, 7)
+	g.addEdge(a1, c3, 9)
+	g.addEdge(a1, f6, 14)
+	g.addEdge(b2, c3, 10)
+	g.addEdge(b2, d4, 15)
+	g.addEdge(c3, d4, 11)
+	g.addEdge(f6, c3, 2)
+	g.addEdge(e5, f6, 9)
+	g.addEdge(d4, e5, 6)
 
 	// fmt.Println("Узел в графе по имени:", g.nodes["a"])
 	// fmt.Println("Статус узла в графе по имени:", g.nodes["a"].status)
@@ -105,6 +158,7 @@ func main() {
 	// fmt.Println(g.nodes["b"], g.nodes["b"].status)
 	// fmt.Println("Весь граф:", g)
 
-	g.dijkstra(a)
+	g.dijkstra(a1)
+	fmt.Println("Весь граф:", g)
 
 }
